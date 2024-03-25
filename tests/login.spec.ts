b9a435c6e0b251cf7a167b8b15c3134a4451f07e
Maciej Mazoń <maciej.mazon@gmail.com>
2024-03-25 17:03:44 +0100

    New tests: Check saving account details and Log out from the desktop

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,12 +1,13 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('User login to Demobank', () => {
+      // Arrange
+      const url = 'https://demo-bank.vercel.app/'
+      const login = 'AndrzejD'
+      const password = 'test123@'
+      const expectedUsername = 'Jan Demobankowy'
   test('Successful login with correct creditential', async ({ page }) => {
-    // Arrange
-    const url = 'https://demo-bank.vercel.app/'
-    const login = 'AndrzejD'
-    const password = 'test123@'
-    const expectedUsername = 'Jan Demobankowy'
+
     // Act
     await page.goto(url);
     await page.getByTestId('login-input').fill(login);
@@ -18,20 +19,20 @@ test.describe('User login to Demobank', () => {
   });
 
   test('Unsuccessful login with too short username', async ({ page }) => {
-    await page.goto('https://demo-bank.vercel.app/');
+    // Act
+    await page.goto(url);
     await page.getByTestId('login-input').fill('tester');
     await page.getByTestId('password-input').click();
-    
+    // Assert
     await expect(page.getByTestId('error-login-id')).toHaveText('identyfikator ma min. 8 znaków');
   })
   test('Unsuccessful login with too short password', async ({ page }) => {
-    await page.goto('https://demo-bank.vercel.app/');
-    await page.getByTestId('login-input').fill('AndrzejD');
+    // Act
+    await page.goto(url);
+    await page.getByTestId('login-input').fill(login);
     await page.getByTestId('password-input').fill('12345');
     await page.getByTestId('password-input').blur();
-
+    // Assert
     await expect(page.getByTestId('error-login-password')).toHaveText('hasło ma min. 8 znaków');
   })
-
-})
-  ;
+});
--- tests/pulpit.spec.ts
+++ tests/pulpit.spec.ts
@@ -2,75 +2,100 @@ import { test, expect } from '@playwright/test';
 
 test.describe('Pulpit tests', () => { 
       // Arrange
-      test.beforeEach(async ({page}) => {
-      const url = 'https://demo-bank.vercel.app/';
-      await page.goto(url)  
-    });
-      const url = 'https://demo-bank.vercel.app/'
-      const login = 'AndrzejD'
-      const password = 'test123@'
-      const reciverID = '2'
-      const transferAmount = '100'
-      const transferTitle = 'Zwrot środków'
-      const expectedtransferReciver = 'Chuck Demobankowy'
-      const accountOwner = 'Jan Demobankowy'
-      test('quick payment with correct data', async ({ page }) => {
+  test.beforeEach(async ({page}) => {
+    const url = 'https://demo-bank.vercel.app/';
+    await page.goto(url)  
+  });
+    const url = 'https://demo-bank.vercel.app/'
+    const login = 'AndrzejD'
+    const password = 'test123@'
+    const reciverID = '2'
+    const transferAmount = '100'
+    const transferTitle = 'Zwrot środków'
+    const expectedtransferReciver = 'Chuck Demobankowy'
+    const accountOwner = 'Jan Demobankowy'
 
-      // Act
-      await page.getByTestId('login-input').fill(login);
-      await page.getByTestId('password-input').fill(password);
-      await page.getByTestId('login-button').click();
+  test('Quick payment with correct data', async ({ page }) => {
+    // Act
+    await page.getByTestId('login-input').fill(login);
+    await page.getByTestId('password-input').fill(password);
+    await page.getByTestId('login-button').click();
       
-      await page.locator('#widget_1_transfer_receiver').selectOption(reciverID);
-      await page.locator('#widget_1_transfer_amount').fill(transferAmount);
-      await page.locator('#widget_1_transfer_title').fill(transferTitle);
+    await page.locator('#widget_1_transfer_receiver').selectOption(reciverID);
+    await page.locator('#widget_1_transfer_amount').fill(transferAmount);
+    await page.locator('#widget_1_transfer_title').fill(transferTitle);
 
-      await page.locator('#execute_btn').click();
-      await page.getByTestId('close-button').click();
-      // Assert  
-      await expect(page.locator('#show_messages')).toHaveText(`Przelew wykonany! ${expectedtransferReciver} - ${transferAmount},00PLN - ${transferTitle}`);
-    });
-      // Act
-      test('quick phone recharge with correct data', async ({ page }) => {
-      await page.getByTestId('login-input').fill(login);
-      await page.getByTestId('password-input').fill(password);
-      await page.getByTestId('login-button').click();
+    await page.locator('#execute_btn').click();
+    await page.getByTestId('close-button').click();
+    // Assert  
+    await expect(page.locator('#show_messages')).toHaveText(`Przelew wykonany! ${expectedtransferReciver} - ${transferAmount},00PLN - ${transferTitle}`);
+  });
 
-      await page.locator('#widget_1_topup_receiver').selectOption('502 xxx xxx');
-      await page.locator('#widget_1_topup_amount').fill(transferAmount);
-      await page.locator('#uniform-widget_1_topup_agreement span').click();
-      await page.getByRole('button', { name: 'doładuj telefon' }).click();
-      await page.getByTestId('close-button').click();
+  test('Quick phone recharge with correct data', async ({ page }) => {
+    // Act
+    await page.getByTestId('login-input').fill(login);
+    await page.getByTestId('password-input').fill(password);
+    await page.getByTestId('login-button').click();
 
-      await expect(page.locator('#show_messages')).toHaveText(`Doładowanie wykonane! ${transferAmount},00PLN na numer 502 xxx xxx`);
-    });
+    await page.locator('#widget_1_topup_receiver').selectOption('502 xxx xxx');
+    await page.locator('#widget_1_topup_amount').fill(transferAmount);
+    await page.locator('#uniform-widget_1_topup_agreement span').click();
+    await page.getByRole('button', { name: 'doładuj telefon' }).click();
+    await page.getByTestId('close-button').click();
+    // Assert
+    await expect(page.locator('#show_messages')).toHaveText(`Doładowanie wykonane! ${transferAmount},00PLN na numer 502 xxx xxx`);
+  });
 
-      test('click to see account details', async ({ page }) => {
+  test('Click to see account details', async ({ page }) => {
+    // Act
+    await page.getByTestId('login-input').fill(login);
+    await page.getByTestId('password-input').fill(password);
+    await page.getByTestId('login-button').click();
 
-      // Act
-      await page.getByTestId('login-input').fill(login);
-      await page.getByTestId('password-input').fill(password);
-      await page.getByTestId('login-button').click();
+    await page.locator('#accounts_list').getByText('więcej').click();
+    await page.locator('#owner').click();
 
-      await page.locator('#accounts_list').getByText('więcej').click();
-      await page.locator('#owner').click();
+    // Assert
+    await expect(page.locator('#owner')).toHaveText(`${accountOwner}`); 
+  });
 
-      // Assert
-      await expect(page.locator('#owner')).toHaveText(`${accountOwner}`); 
-    });
+  test('Check receipts and expenses', async ({ page }) => {
 
-      test('click to see receipts and expenses', async ({ page }) => {
+    // Act
+    await page.getByTestId('login-input').fill(login);
+    await page.getByTestId('password-input').fill(password);
+    await page.getByTestId('login-button').click();
 
-      // Act
-      await page.getByTestId('login-input').fill(login);
-      await page.getByTestId('password-input').fill(password);
-      await page.getByTestId('login-button').click();
+    await page.locator('form').filter({ hasText: 'manager finansowy wpływy i' }).getByRole('combobox').selectOption('1');
 
-      await page.locator('form').filter({ hasText: 'manager finansowy wpływy i' }).getByRole('combobox').selectOption('1');
+    // Assert
+    await expect(page.locator('form').filter({ hasText: 'manager finansowy wpływy i' })).toHaveClass(`box-white widget`);
+  });
 
-      // Assert
-      await expect(page.locator('form').filter({ hasText: 'manager finansowy wpływy i' })).toHaveClass(`box-white widget`);
-    });
+  test('Check saving account details', async ({ page }) => {
+    // Act
+    await page.getByTestId('login-input').fill(login);
+    await page.getByTestId('password-input').fill(password);
+    await page.getByTestId('login-button').click();
 
-    
+    await page.locator('article').filter({ hasText: 'TEStos (22 xxxx xxxx xxxx' }).locator('a').click();
+
+    // Assert
+    await expect(page.getByText('Progress')).toHaveId(""); 
+  });
+
+  test('Log out from the desktop', async ({ page }) => {
+
+    // Act
+    await page.getByTestId('login-input').fill(login);
+    await page.getByTestId('password-input').fill(password);
+    await page.getByTestId('login-button').click();
+
+    await page.getByTestId('logout-button').click();
+    await page.getByRole('heading', { name: 'Wersja demonstracyjna serwisu' }).click();
+
+    // Assert
+    await expect(page.getByRole('heading', {name: 'Wersja demonstracyjna serwisu'})).toHaveText(`Wersja demonstracyjna serwisu Demobank`);
+  });
+  
 })
